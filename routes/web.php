3f968a94a96f5c0d61003f789b24ca867af1ed5b
mikolajgawronski <gawronski.miko@gmail.com>
2021-08-27 01:07:30 +0200

    #6 - Removed unnecessary route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@ $router = app()->make(Router::class);
 
 $router->get("/", [CityController::class, "index"])->name("home");
 $router->get("/create", [CityController::class, "create"]);
-$router->get("/dupa", [CityController::class, "dupa"]);
 $router->post("/create", [CityController::class, "store"])->name("create.store");
 $router->get("/{id}", [WeatherController::class, "index"]);
